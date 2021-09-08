3146fdcc9ada92efffdde65d8b77888c7af1d549
Varun Sunil Pathak <83233491+pathakvsp1@users.noreply.github.com>
2021-09-08 17:27:48 +0530

    Add files via upload

---
+++ internNow/client/src/components/screens/SubscribesUserPosts.js
@@ -0,0 +1,182 @@
+import React,{useState,useEffect,useContext} from 'react'
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
+const Home  = ()=>{
+    const [data,setData] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(()=>{
+       fetch('/getsubpost',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           setData(result.posts)
+       })
+    },[])
+
+    const likePost = (id)=>{
+          fetch('/like',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId:id
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+                   //   console.log(result)
+            const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+          }).catch(err=>{
+              console.log(err)
+          })
+    }
+    const unlikePost = (id)=>{
+          fetch('/unlike',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId:id
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+            //   console.log(result)
+            const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+          }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const makeComment = (text,postId)=>{
+          fetch('/comment',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId,
+                  text
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+              console.log(result)
+              const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+             })
+            setData(newData)
+          }).catch(err=>{
+              console.log(err)
+          })
+    }
+
+    const deletePost = (postid)=>{
+        fetch(`/deletepost/${postid}`,{
+            method:"delete",
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.filter(item=>{
+                return item._id !== result._id
+            })
+            setData(newData)
+        })
+    }
+   return (
+       <div className="home">
+           {
+               data.map(item=>{
+                   return(
+                       <div className="card home-card" key={item._id}>
+                            <h5 style={{padding:"5px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile"  }>{item.postedBy.name}</Link> {item.postedBy._id === state._id 
+                            && <i className="material-icons" style={{
+                                float:"right",
+                                paddingRight: "15px"
+                            }} 
+                            onClick={()=>deletePost(item._id)}
+                            >delete</i>
+
+                            }</h5>
+                            <div className="card-image">
+                                <img src={item.photo} alt="pic"/>
+                            </div>
+                            <div className="card-content">
+                            {item.likes.includes(state._id)
+                            ? 
+                             <i className="material-icons" style={{color:"red"}}
+                                    onClick={()=>{unlikePost(item._id)}}
+                              >favorite</i>
+                            : 
+                            <i className="material-icons"
+                            onClick={()=>{likePost(item._id)}}
+                            >favorite_border</i>
+                            }&nbsp;&nbsp;
+                            <i className="material-icons"
+                            onClick={()=>{
+                                {var cid = "comment";}
+                                {cid+=item._id}                                
+                                document.getElementById(cid).style.display = "block";
+                            }}
+                            >comment</i>
+                           
+                                <h6>{item.likes.length} likes</h6>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <div id={"comment" + item._id} style={{display:"none"}}>
+                                {
+                                    item.comments.map(record=>{
+                                        return(
+                                        <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                                        )
+                                    })
+                                }
+                                </div>
+                                <hr/>                                
+                                <form className="comment" onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item._id)
+                                    e.target[0].value=""
+                                }}>
+                                  <input type="text" placeholder="Add a Comment" />  
+                                </form>
+                                
+                            </div>
+                        </div> 
+                   )
+               })
+           }
+          
+          
+       </div>
+   )
+}
+
+
+export default Home
